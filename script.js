0ed3cafcba654255d8f32b21b96c147cb24e3551
u202314254 <malexandromc@gmail.com>
2024-10-31 02:25:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Landing page");
+document.querySelectorAll('.question h3').forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
